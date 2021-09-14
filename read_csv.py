f6872eaedbf4c356ceacf26b3ca9c73b6dbac7b2
Raphael <raphaelrcbarbosa@gmail.com>
2021-09-14 18:37:31 -0300

    Open csv file and read rows

--- read_csv.py
+++ read_csv.py
@@ -1,1 +1,23 @@
+
+import csv
+
 print ("hello world")
+
+file = open ("run6.csv")
+
+csvreader = csv.reader(file)
+
+cabecalho = []
+cabecalho = next(csvreader)
+
+while (cabecalho[0][0] != 'X'):#Procura pela definição dos dados no arquivo.
+    cabecalho = next(csvreader)
+
+print(cabecalho)
+
+rows = []
+for row in csvreader:
+        rows.append(row)
+# print(rows[0])
+
+file.close()
