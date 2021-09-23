c3d1a4d918e7199aa2df3064891dc8b7ae831fb6
Raphael <raphaelrcbarbosa@gmail.com>
2021-09-23 18:52:09 -0300

    New file just to read euler from csv

--- read_csv_E.py
+++ read_csv_E.py
@@ -1,3 +1,6 @@
+###     Programa para abrir e separar um .csv em diferentes arrays. Depende da organização do csv e caso tenha um número diferente de colunas, deverá ser levemente alterado
+###     Lógica a ser usada em cósgis futuros
+###
 
 import csv
 import matplotlib.pyplot as plt
@@ -5,7 +8,7 @@ import math
 
 print ("Iniciando leitura do arquivo")
 
-file = open ("run8.csv")
+file = open ("teste_euler.csv")
 
 csvreader = csv.reader(file)
 
@@ -30,17 +33,13 @@ file.close()
 emg_value = []
 emg_time = []
 
-QuatW_value = []
-QuatX_value = []
-QuatY_value = []
-QuatZ_value = []
-QuatAcc_value = []
+PitchX_value = []
+RollY_value = []
+YawZ_value = []
 
-QuatW_time = []
-QuatX_time = []
-QuatY_time = []
-QuatZ_time = []
-QuatAcc_time = []
+PitchX_time = []
+RollY_time = []
+YawZ_time = []
 
 
 list_mover = []
@@ -60,27 +59,50 @@ for i in rows:
     emg_value.append(list_mover)
     if (i[2] != ''):
         list_mover = float(i[2])
-        QuatW_time.append(list_mover)
+        PitchX_time.append(list_mover)
     if (i[3] != ''):
         list_mover = float(i[3])
-        QuatW_value.append(list_mover)
+        PitchX_value.append(list_mover)
+    if (i[4] != ''):
+        list_mover = float(i[4])
+        RollY_time.append(list_mover)
+    if (i[5] != ''):
+        list_mover = float(i[5])
+        RollY_value.append(list_mover)
+    if (i[6] != ''):
+        list_mover = float(i[6])
+        YawZ_time.append(list_mover)
+    if (i[7] != ''):
+        list_mover = float(i[7])
+        YawZ_value.append(list_mover)
 
 # print(len(emg_value))
-print("time: ", emg_time[0] ,"s Emg = ", emg_value[0], "V")
+# print("time: ", emg_time[0] ,"s Emg = ", emg_value[0], "V")
 
 plt.figure()
 plt.plot(emg_time, emg_value,label='EMG')
-plt.plot(QuatW_time, QuatW_value, label='Quaternion W')    
-# plt.plot(tempo, QuaternionZ, label='Quaternion Z')
-# plt.plot(tempo, QuaternionW, label='Quaternion W')
 plt.legend()
 plt.grid()
 plt.show()
 
+plt.figure()
+plt.plot(PitchX_time, PitchX_value, label='Pitch X')
+plt.legend()
+plt.grid()
+plt.show()
 
-# print(emg_list[1])
-# print(emg_list[2])
-# print(emg_list[21599])
+plt.figure()
+plt.plot(RollY_time, RollY_value, label='Roll Y')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.plot(YawZ_time, YawZ_value, label='Yaw Z')
+plt.legend()
+plt.grid()
+plt.show()
+
 #Transformando rows em arrays para melhor manipulação.
 
 
